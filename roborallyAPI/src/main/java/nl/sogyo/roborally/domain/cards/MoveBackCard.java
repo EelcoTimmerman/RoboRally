5016c8206b219635b04022bd1dab8275a8b126a2
SaidHome <sekhouani@sogyo.nl>
2020-04-10 15:30:09 +0200

    movebackcard added

---
+++ roborallyAPI/src/main/java/nl/sogyo/roborally/domain/cards/MoveBackCard.java
@@ -0,0 +1,11 @@
+package nl.sogyo.roborally.domain.cards;
+
+import nl.sogyo.roborally.domain.Direction;
+
+public class MoveBackCard {
+
+    public void moveOneBack(Direction direction) {
+
+    }
+
+}
