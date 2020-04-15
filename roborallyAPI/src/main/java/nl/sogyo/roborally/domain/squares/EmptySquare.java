97656220fcf0c184a6c77e011e1cb83284a9beea
agoldschmidt <agoldschmidt@sogyo.nl>
2020-04-15 09:17:30 +0200

    reverting back to previous commit for build success

--- roborallyAPI/src/main/java/nl/sogyo/roborally/domain/squares/EmptySquare.java
+++ roborallyAPI/src/main/java/nl/sogyo/roborally/domain/squares/EmptySquare.java
@@ -2,7 +2,6 @@ package nl.sogyo.roborally.domain.squares;
 
 public class EmptySquare extends Square {
 
-<<<<<<< HEAD
 //    public EmptySquare(){
 //
 //    }
@@ -13,17 +12,6 @@ public class EmptySquare extends Square {
 //        this.southWall = southWall;
 //        this.westWall = westWall;
 //    }
-=======
-    public EmptySquare(){
-    }
-
-    public EmptySquare(boolean northWall, boolean eastWall, boolean southWall, boolean westWall){
-        this.northWall = northWall;
-        this.eastWall = eastWall;
-        this.southWall = southWall;
-        this.westWall = westWall;
-    }
->>>>>>> branch 'master' of git@git.sogyo.nl:academy/projects/other/roborally.git
 
     public Square getDestination(){
         return this;
--- roborallyAPI/src/test/java/nl/sogyo/roborally/domain/robots/TestRobot.java
+++ roborallyAPI/src/test/java/nl/sogyo/roborally/domain/robots/TestRobot.java
@@ -2,75 +2,30 @@ package nl.sogyo.roborally.domain.robots;
 
 import org.junit.Test;
 
-import nl.sogyo.roborally.domain.Direction;
-import nl.sogyo.roborally.domain.squares.EmptySquare;
 // import nl.sogyo.roborally.domain.Direction;
-import nl.sogyo.roborally.domain.squares.Square;
+import nl.sogyo.roborally.domain.squares.EmptySquare;
 
 public class TestRobot {
 
     @Test
-    public void TestGetStartPosition() {
-        EmptySquare startSquare = new EmptySquare(true, false, false, false);
+    public void TestGetStartPosition(){
+        EmptySquare startSquare = new EmptySquare();
         Robot robot = new Robot(startSquare);
         assert(robot.getPosition() == startSquare);
     }
 
     @Test
-    public void TestIsAtSquare() {
-        EmptySquare startSquare = new EmptySquare(true, false, false, false);
+    public void TestIsAtSquare(){
+        EmptySquare startSquare = new EmptySquare();
         Robot robot = new Robot(startSquare);
         assert(robot.isAt(startSquare));
     }
 
     @Test
-    public void TestTakeDamage() {
-        EmptySquare startSquare = new EmptySquare(true, false, false, false);
+    public void TestTakeDamage(){
+        EmptySquare startSquare = new EmptySquare();
         Robot robot = new Robot(startSquare);
         robot.takeDamage(1);
         assert(robot.getHealth() == 8);
-    }
-
-    @Test
-    public void TestMoveOneNorth() {
-        Square startSquare = new EmptySquare();
-        Square northOfStartSquare = new EmptySquare();
-        Robot robot = new Robot(startSquare);
-        startSquare.setNorthNeighbour(northOfStartSquare);
-        robot.moveForward();
-        assert(robot.isAt(northOfStartSquare));
-    }
-
-    @Test
-    public void TestMoveOneEast() {
-        Square startSquare = new EmptySquare();
-        Square eastOfStartSquare = new EmptySquare();
-        Robot robot = new Robot(startSquare);
-        robot.setOrientation(Direction.EAST);
-        startSquare.setEastNeighbour(eastOfStartSquare);
-        robot.moveForward();
-        assert(robot.isAt(eastOfStartSquare));
-    }
-
-    @Test
-    public void TestMoveOneSouth() {
-        Square startSquare = new EmptySquare();
-        Square southOfStartSquare = new EmptySquare();
-        Robot robot = new Robot(startSquare);
-        robot.setOrientation(Direction.SOUTH);
-        startSquare.setSouthNeighbour(southOfStartSquare);
-        robot.moveForward();
-        assert(robot.isAt(southOfStartSquare));
-    }
-
-    @Test
-    public void TestMoveOneWest() {
-        Square startSquare = new EmptySquare();
-        Square westOfStartSquare = new EmptySquare();
-        Robot robot = new Robot(startSquare);
-        robot.setOrientation(Direction.WEST);
-        startSquare.setWestNeighbour(westOfStartSquare);
-        robot.moveForward();
-        assert(robot.isAt(westOfStartSquare));
     }
 }
