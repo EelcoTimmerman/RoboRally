f49401f4237f13565501a620b4c560c9b058ebd2
Joris Bink <j.c.bink@students.uu.nl>
2020-05-06 16:56:47 +0200

    Implement moveThreeCard and MoveBackCard
    No tests, since they implement the same logic as the other movecards

--- roborallyAPI/src/main/java/nl/sogyo/roborally/domain/cards/MoveBackCard.java
+++ roborallyAPI/src/main/java/nl/sogyo/roborally/domain/cards/MoveBackCard.java
@@ -4,29 +4,14 @@ import java.util.List;
 
 import nl.sogyo.roborally.domain.robots.Robot;
 import nl.sogyo.roborally.domain.squares.Board;
-import nl.sogyo.roborally.domain.squares.Pit;
-import nl.sogyo.roborally.domain.squares.Square;
 
 public class MoveBackCard extends Card{
 
     public void doCardAction(Robot robot, Board board, List<Robot> robots){
-        if(canMoveBackwards(robot, board)) robot.moveBackwards();    
-        if(robotNotOnBoard(robot, board) || robotInPit(robot, board)) robot.respawn();
+        moveRobotInDirectionIfPossible(robot, robot.getOrientation().getReverse(), board, robots);
+        respawnIfNecessary(robot, board);
     }
 
-    private boolean robotInPit(Robot robot, Board board){
-        Square currentPosition = board.getSquare(robot.getXCoordinate(), robot.getYCoordinate());
-        return (currentPosition instanceof Pit);
-    }
-
-    private boolean robotNotOnBoard(Robot robot, Board board){
-        return robot.getXCoordinate() < 0 || robot.getYCoordinate() < 0 || robot.getXCoordinate() >= board.getWidth() || robot.getYCoordinate() >= board.getHeight();
-    }
-
-    private boolean canMoveBackwards(Robot robot, Board board){
-        Square currentPosition = board.getSquare(robot.getXCoordinate(), robot.getYCoordinate());
-        return !currentPosition.hasWallAt(robot.getOrientation().getReverse());
-    }
 
     @Override
     public int getSpeed(){
--- roborallyAPI/src/main/java/nl/sogyo/roborally/domain/cards/MoveThreeCard.java
+++ roborallyAPI/src/main/java/nl/sogyo/roborally/domain/cards/MoveThreeCard.java
@@ -4,37 +4,23 @@ import java.util.List;
 
 import nl.sogyo.roborally.domain.robots.Robot;
 import nl.sogyo.roborally.domain.squares.Board;
-import nl.sogyo.roborally.domain.squares.Pit;
-import nl.sogyo.roborally.domain.squares.Square;
 
 public class MoveThreeCard extends Card{
 
-    public void doCardAction(Robot robot, Board board, List<Robot> robots){      
-        if(canMoveForward(robot, board)) robot.moveForward();    
-        if(robotNotOnBoard(robot, board) || robotInPit(robot, board)) robot.respawn();
-        else{
-            if(canMoveForward(robot, board)) robot.moveForward();    
-            if(robotNotOnBoard(robot, board) || robotInPit(robot, board)) robot.respawn();
-            else{
-                if(canMoveForward(robot, board)) robot.moveForward();    
-                if(robotNotOnBoard(robot, board) || robotInPit(robot, board)) robot.respawn();
+    public void doCardAction(Robot robot, Board board, List<Robot> robots){
+        boolean hasMoved = moveRobotInDirectionIfPossible(robot, robot.getOrientation(), board, robots);
+        boolean hasRespawned = respawnIfNecessary(robot, board);
+        if(hasMoved && !hasRespawned){
+            hasMoved = moveRobotInDirectionIfPossible(robot, robot.getOrientation(), board, robots);
+            hasRespawned = respawnIfNecessary(robot, board);
+            
+            if(hasMoved && !hasRespawned){
+                moveRobotInDirectionIfPossible(robot, robot.getOrientation(), board, robots);
+                respawnIfNecessary(robot, board);
             }
         }
     }
 
-    private boolean robotInPit(Robot robot, Board board){
-        Square currentPosition = board.getSquare(robot.getXCoordinate(), robot.getYCoordinate());
-        return (currentPosition instanceof Pit);
-    }
-    
-    private boolean robotNotOnBoard(Robot robot, Board board){
-        return robot.getXCoordinate() < 0 || robot.getYCoordinate() < 0 || robot.getXCoordinate() >= board.getWidth() || robot.getYCoordinate() >= board.getHeight();
-    }
-
-    private boolean canMoveForward(Robot robot, Board board){
-        Square currentPosition = board.getSquare(robot.getXCoordinate(), robot.getYCoordinate());
-        return !currentPosition.hasWallAt(robot.getOrientation());
-    }
 
     @Override
     public int getSpeed(){
