07a7fbb508878b08a8f7f8b3fb60705af396d865
Joris Bink <j.c.bink@students.uu.nl>
2020-05-06 16:48:05 +0200

    Move many methods from moveonecard to Card

--- roborallyAPI/src/main/java/nl/sogyo/roborally/domain/cards/Card.java
+++ roborallyAPI/src/main/java/nl/sogyo/roborally/domain/cards/Card.java
@@ -2,12 +2,55 @@ package nl.sogyo.roborally.domain.cards;
 
 import java.util.List;
 
+import nl.sogyo.roborally.domain.Direction;
 import nl.sogyo.roborally.domain.robots.Robot;
-import nl.sogyo.roborally.domain.squares.Board;
+import nl.sogyo.roborally.domain.squares.*;
 
 public abstract class Card{
 
     public abstract void doCardAction(Robot robot, Board board, List<Robot> robots);
 
     public abstract int getSpeed();
+    
+    protected boolean moveRobotInDirectionIfPossible(Robot robot, Direction direction, Board board, List<Robot> otherRobots){
+        boolean hasMoved = true;
+        boolean isBlockedByWall = checkForWall(robot, direction, board);
+        if(!isBlockedByWall){
+            robot.move(direction);
+            for(Robot otherRobot : otherRobots){
+                if(otherRobot.isAt(robot.getXCoordinate(), robot.getYCoordinate()) && otherRobot != robot){
+                    hasMoved &= moveRobotInDirectionIfPossible(otherRobot, direction, board, otherRobots);
+                    if(hasMoved){
+                        respawnIfNecessary(otherRobot, board);
+                    }
+                    else{
+                        robot.move(direction.getReverse());
+                    }
+                    break;
+                }
+            }
+        }
+        else{
+            hasMoved = false;
+        }
+        return hasMoved;
+    }
+    
+    private boolean checkForWall(Robot robot, Direction direction, Board board){
+        Square currentPosition = board.getSquare(robot.getXCoordinate(), robot.getYCoordinate());
+        return currentPosition.hasWallAt(direction);
+    }
+    
+    protected void respawnIfNecessary(Robot robot, Board board){        
+        if(robotNotOnBoard(robot, board) || robotInPit(robot, board)) robot.respawn();
+    }
+    
+    private boolean robotNotOnBoard(Robot robot, Board board){
+        return robot.getXCoordinate() < 0 || robot.getYCoordinate() < 0 || robot.getXCoordinate() >= board.getWidth() || robot.getYCoordinate() >= board.getHeight();
+    }
+    
+    private boolean robotInPit(Robot robot, Board board){
+        Square currentPosition = board.getSquare(robot.getXCoordinate(), robot.getYCoordinate());
+        return (currentPosition instanceof Pit);
+    }
 }
--- roborallyAPI/src/main/java/nl/sogyo/roborally/domain/cards/MoveOneCard.java
+++ roborallyAPI/src/main/java/nl/sogyo/roborally/domain/cards/MoveOneCard.java
@@ -2,59 +2,14 @@ package nl.sogyo.roborally.domain.cards;
 
 import java.util.List;
 
-import nl.sogyo.roborally.domain.Direction;
 import nl.sogyo.roborally.domain.robots.Robot;
 import nl.sogyo.roborally.domain.squares.Board;
-import nl.sogyo.roborally.domain.squares.Pit;
-import nl.sogyo.roborally.domain.squares.Square;
 
 public class MoveOneCard extends Card{
 
     public void doCardAction(Robot robot, Board board, List<Robot> robots){
         moveRobotInDirectionIfPossible(robot, robot.getOrientation(), board, robots);
         respawnIfNecessary(robot, board);
-    }
-
-    private boolean robotInPit(Robot robot, Board board){
-        Square currentPosition = board.getSquare(robot.getXCoordinate(), robot.getYCoordinate());
-        return (currentPosition instanceof Pit);
-    }
-
-    private boolean robotNotOnBoard(Robot robot, Board board){
-        return robot.getXCoordinate() < 0 || robot.getYCoordinate() < 0 || robot.getXCoordinate() >= board.getWidth() || robot.getYCoordinate() >= board.getHeight();
-    }
-
-    private boolean moveRobotInDirectionIfPossible(Robot robot, Direction direction, Board board, List<Robot> otherRobots){
-        boolean hasMoved = true;
-        boolean isBlockedByWall = checkForWall(robot, direction, board);
-        if(!isBlockedByWall){
-            robot.move(direction);
-            for(Robot otherRobot : otherRobots){
-                if(otherRobot.isAt(robot.getXCoordinate(), robot.getYCoordinate()) && otherRobot != robot){
-                    hasMoved &= moveRobotInDirectionIfPossible(otherRobot, direction, board, otherRobots);
-                    if(hasMoved){
-                        respawnIfNecessary(otherRobot, board);
-                    }
-                    else{
-                        robot.move(direction.getReverse());
-                    }
-                    break;
-                }
-            }
-        }
-        else{
-            hasMoved = false;
-        }
-        return hasMoved;
-    }
-
-    private boolean checkForWall(Robot robot, Direction direction, Board board){
-        Square currentPosition = board.getSquare(robot.getXCoordinate(), robot.getYCoordinate());
-        return currentPosition.hasWallAt(direction);
-    }
-
-    private void respawnIfNecessary(Robot robot, Board board){        
-        if(robotNotOnBoard(robot, board) || robotInPit(robot, board)) robot.respawn();
     }
 
     @Override
