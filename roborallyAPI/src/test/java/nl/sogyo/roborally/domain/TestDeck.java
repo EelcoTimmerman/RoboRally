a6537fcab7f7c4bfae1fd27e18b03e103f1c42a2
Eelco Timmerman <eelco.timmerman@live.nl>
2020-05-07 10:50:21 +0200

    minor change to have liveshare up to date

--- roborallyAPI/src/test/java/nl/sogyo/roborally/domain/TestDeck.java
+++ roborallyAPI/src/test/java/nl/sogyo/roborally/domain/TestDeck.java
@@ -1,27 +1,35 @@
 package nl.sogyo.roborally.domain;
 
 import nl.sogyo.roborally.domain.cards.Deck;
-
-import java.util.List;
-
 import org.junit.Test;
 import nl.sogyo.roborally.domain.cards.*;
+import java.util.List;
 
 public class TestDeck {
     
     @Test
+    public void TestCreateDeck1(){
+        Deck deck = new Deck();
+        deck.createDeck();
+        List<ICard> cardsInHand = deck.getHand(0);
+        assert(cardsInHand.size() == 9);
+        assert(deck.getSize() == 84-9);
+    }
+
+    @Test
     public void TestCreateDeck(){
         Deck deck = new Deck();
         deck.createDeck();
-        assert(deck.getSize() == 84);  
+        List<ICard> cardsInHand = deck.getHand(0);
+        assert(cardsInHand.size() == 9);
+        assert(deck.getSize() == 84-9);
     }
 
     @Test
     public void TestCreateDeck2(){
         Deck deck = new Deck();
         deck.createDeck();
-        List<ICard> cardsInHand = deck.getHand(0);
-        assert(cardsInHand.size() == 9);
+        assert(deck.getSize() == 84);
     }
 
 
