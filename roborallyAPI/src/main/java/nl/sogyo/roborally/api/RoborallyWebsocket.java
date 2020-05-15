e23b53c3af12b7e5a4dcef50e2ae8e5d56f44e4a
Joris Bink <j.c.bink@students.uu.nl>
2020-05-15 11:53:49 +0200

    Removed some changes that didnt belong in this issue

--- roborallyAPI/src/main/java/nl/sogyo/roborally/api/RoborallyWebsocket.java
+++ roborallyAPI/src/main/java/nl/sogyo/roborally/api/RoborallyWebsocket.java
@@ -51,10 +51,8 @@ public class RoborallyWebsocket{
         }
         else{
             int cardnr = Integer.parseInt(message);
-            //future: int[] cardnrs = Integer.parseInt(message);
-            int[] cardnrs = {cardnr,3,3,3,3};
             Robot robot = robots.get(session);
-            robot.program(cardnrs);
+            robot.program(cardnr);
             roborally.playRoundIfAllRobotsReady();
         }
         updateAllPlayers();
